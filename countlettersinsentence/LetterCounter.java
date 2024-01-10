253bd5b66c43b2911b47b4e70c1255925ceb4af4
mdsimms91 <mdsimms91@a.a>
2024-01-10 18:07:31 -0500

    c

---
+++ countlettersinsentence/LetterCounter.java
@@ -0,0 +1,29 @@
+package com.skillstorm.countlettersinsentence;
+
+import java.util.HashMap;
+
+public class LetterCounter {
+	public static void main(String[] args) {
+		String sentence1 = "The quick brown fox jumps over the lazy dog.";
+		String sentence2 = "Testing 1 2 3 4 ... testing 1 2 3 4";
+		
+		System.out.println("First Sentence: " + countLetters(sentence1));
+		System.out.println("Second Sentence: " + countLetters(sentence2));
+	}
+	
+	public static HashMap<Character, Integer> countLetters(String str) {
+	    
+		HashMap<Character, Integer> letterCounter = new HashMap<>();
+	    for(Character c : str.replaceAll("\\p{P}|\\s|\\d", "").toLowerCase().toCharArray()) {
+	    	if(letterCounter.containsKey(c)) {
+	    		Integer tempCount = letterCounter.get(c);
+	    		letterCounter.replace(c, tempCount + 1);
+	    	}else {
+	    		letterCounter.put(c, 1);
+	    	}
+	    }
+	    
+	    return letterCounter;
+	    
+	}
+}
--- rgb/Test.java
+++ rgb/Test.java
@@ -5,7 +5,6 @@ public class Test {
 	public static void main(String[] args) {
 		Student s1 = new Student();
 		Student s2 = new Student();
-		System.out.println((int)(Math.random() * 100));
 		System.out.println(s1.equals(s2));
 		
 		System.out.println(s1.hashCode());
